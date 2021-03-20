5d1b1f403595034feed76c4fa90414fd1c2f85d4
SamuelYudiGunawan <samuelyudigun@gmail.com>
2021-03-20 13:16:05 +0700

    initial commit

---
+++ app/src/main/java/com/example/ptsgenap10rpl129/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ptsgenap10rpl129;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonlogin = findViewById(R.id.btnLogin);
+
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+
+    void login(){
+        EditText usernameLogin = (EditText)findViewById(R.id.username);
+        EditText passwordLogin = (EditText)findViewById(R.id.password);
+
+        String username = "Samuel Yudi";
+        String password = "test";
+
+        String emailLog = usernameLogin.getText().toString();
+        String passwordLog = passwordLogin.getText().toString();
+
+        //toast = alert
+
+        if (emailLog.equals(username) && passwordLog.equals(password)) {
+            Toast.makeText(this, "Anda Berhasil Masuk", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Username atau Password Salah", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
